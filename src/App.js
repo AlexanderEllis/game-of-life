9e5fd4f4522eccd19ea40d838643f103d44f7fcb
Alexander Ellis <ellisonjtk@gmail.com>
2017-03-29 11:07:20 -0400

    updated display and methods

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Tile extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.handleClick(this.props.index);
   }
   render() {
-    
+    let type = this.props.type === 1 ? 'alive' : 'dead';
+    return(
+      <div className={type + ' tile'} onClick={this.handleClick}></div>
+    );
   }
 }
 
 class Board extends Component {
   constructor(props) {
     super(props);
+    this.printState = this.printState.bind(this);
   }
   printState() {
-    let size = this.props.size;
     let currentBoard = this.props.board;
     
     let displaying = [];
-    let result = '';
     for (let i = 0; i < currentBoard.length; i++) {
-      let placeholder = currentBoard[i] === 0 ? '_' : '0';
-      result += ' ' + placeholder + ' ';
-      if (i > 0 && i % size === size - 1) {
-        result += ''
-        displaying.push(<p>{result}</p>);
-        result = '';
-      }
+      displaying.push(<Tile type={currentBoard[i]} index={i} handleClick={this.props.handleClick} key={i}/>);
     }
     return displaying; 
   }
   render() {
     let size = this.props.size;
+    let dimensions = size * 14;
     let display = this.printState();
     return (
       <div>
-        <p>We see that there will be a board of {size}</p>
-        <p>here's the board:</p>
-        <p>{display}</p>
+        <p>Generation {this.props.generation}</p>
+        <div id="container" style={{'width': dimensions, 'height': dimensions}}>{display}</div>
       </div>
     )
   }
@@ -48,12 +47,12 @@ class Board extends Component {
 class App extends Component {k
   constructor(props) {
     super(props);
-    const size = 25;
+    const size = 50;
     let firstBoard = this.resetBoard(size);
     let firstState = {
       board: firstBoard,
       size: size,
-      generations: 1,
+      generations: 0,
       running: false
     }
     this.state = firstState;
@@ -64,8 +63,32 @@ class App extends Component {k
     this.toggleGame = this.toggleGame.bind(this);
     this.startGame = this.startGame.bind(this);
     this.resetGame = this.resetGame.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.clearBoard = this.clearBoard.bind(this);
 
     this.startGame();
+  }
+
+  clearBoard() {
+    let blankBoard = new Array(this.state.size * this.state.size).fill(0);
+    this.setState({
+      board: blankBoard,
+      size:this.state.size,
+      generations: 0,
+      running: false
+    });
+  }
+  
+  handleClick(index) {
+    console.log('hey');
+    let currentBoard = this.state.board;
+    currentBoard[index] = currentBoard[index] === 1 ? 0 : 1;
+    this.setState({
+      board: currentBoard,
+      size: this.state.size,
+      generations: this.state.generations,
+      running: this.state.running
+    })
   }
 
   resetBoard(size) {  // Since this resets the state, I'm also going to call it to create the first state.
@@ -109,16 +132,15 @@ class App extends Component {k
   }
   
   createNextBoard() {
-    const width = this.state.size;
     const currentBoard = this.state.board;
     var newBoard = [];
     for (let i = 0; i < currentBoard.length; i++) {
       let localNeighbors = this.neighbors(i);
       let alive = 0;
-      if (currentBoard[i] == 1 && (localNeighbors == 2 || localNeighbors == 3)) {
+      if (currentBoard[i] === 1 && (localNeighbors === 2 || localNeighbors === 3)) {
         alive = 1;
       }
-      else if (currentBoard[i] === 0 && localNeighbors == 3) {
+      else if (currentBoard[i] === 0 && localNeighbors === 3) {
         alive = 1;
       }
       newBoard[i] = alive;
@@ -127,9 +149,8 @@ class App extends Component {k
   } 
 
   startGame() {
-    var interval = setInterval(() => {
+    setInterval(() => {
       if (this.state.running) {
-        let currentBoard = this.state.board;
         let newBoard = this.createNextBoard();
         this.setState({
           board: newBoard,
@@ -168,8 +189,12 @@ class App extends Component {k
         </div>
         <div className="App-intro">
           <button value="start" onClick={this.toggleGame}>Start/Stop</button>
+          <button onClick={this.clearBoard}>Clear</button>
           <button onClick={this.resetGame}>Reset</button>
-          <Board size={this.state.size} board={this.state.board} />
+          <Board size={this.state.size}
+                 board={this.state.board}
+                 generation={this.state.generations}
+                 handleClick={this.handleClick}/>
         </div>
       </div>
     );
